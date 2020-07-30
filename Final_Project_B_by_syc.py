484ad26d105a7b1e09a50706483865d127f80169
SYC-Mirage <67193448+SYC-Mirage@users.noreply.github.com>
2020-07-30 18:15:25 +0800

    Update Final_Project_B_by_syc.py

--- Final_Project_B_by_syc.py
+++ Final_Project_B_by_syc.py
@@ -2,29 +2,29 @@ import pandas as pd
 from efficient_apriori import apriori
 
 # 读取源数据
-raw_dates = pd.read_csv('D:\数据分析学习\数据分析训练营-结营考试\数据分析训练营-结营考试\ProjectB\日期表.csv',sep=',',encoding='gbk')
+raw_guests = pd.read_csv('D:\数据分析学习\数据分析训练营-结营考试\数据分析训练营-结营考试\ProjectB\客户.csv',sep=',',encoding='gbk')
 orders = pd.read_csv('D:\数据分析学习\数据分析训练营-结营考试\数据分析训练营-结营考试\ProjectB\订单表.csv',sep=',',encoding='gbk')
 
-# 将日期取出，以每一天的销售作为一项
-dates = raw_dates['日期']
+# 将客户ID取出，以每一个客户的销售作为一项
+guests = raw_guests['客户ID']
 transactions = []
 print(len(orders))
 
-# 以字典形式存储日期和销售的产品
+# 以字典形式存储客户ID和销售的产品
 data = {}
-for date in dates:
-    data[date] = []
+for guest in guests:
+    data[guest] = []
 
 for i in range(0,len(orders)):
     item = orders.loc[i]
-    data[item['订单日期']].append(item['产品名称'])
+    data[item['客户ID']].append(item['产品名称'])
 
 # 去除重复的产品
-for date in dates:
-    data[date] = set(data[date])
-    transactions.append(data[date])
+for guest in guests:
+    data[guest] = set(data[guest])
+    transactions.append(data[guest])
 
 # 进行关联性分析，经多次尝试确定了min_support和min_confidence合适的取值
-items, rules = apriori(transactions, min_support=0.27,  min_confidence=0.2)
+items, rules = apriori(transactions, min_support=0.08,  min_confidence=0.1)
 print("频繁项集：", items)
 print("关联规则：", rules)
